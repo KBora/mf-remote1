e05ee4bd55e9fefcb1c6662858f43fbd40f30685
Kian Woo <kian.woo@gmail.com>
2022-07-04 22:33:42 +1000

    feat: add material

--- projects/account-master/src/app/account-master/account-master.module.ts
+++ projects/account-master/src/app/account-master/account-master.module.ts
@@ -4,14 +4,21 @@ import { CommonModule } from '@angular/common';
 import { AccountMasterRoutingModule } from './account-master-routing.module';
 import { AccountMasterComponent } from './account-master.component';
 
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     AccountMasterComponent
   ],
   imports: [
     CommonModule,
-    AccountMasterRoutingModule
+    AccountMasterRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ]
 })
 export class AccountMasterModule { }
